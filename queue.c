3164324124577ea14072bd293d1773d441b443e4
s1041562 <910484jim@gmail.com>
2020-09-13 23:06:57 +0800

    Add new member *tail to queue.h

--- queue.c
+++ queue.c
@@ -12,8 +12,12 @@
 queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
-    /* TODO: What if malloc returned NULL? */
+    if (!q) {
+        return NULL;
+    }
     q->head = NULL;
+    q->tail = NULL;
+    q->size = 0;
     return q;
 }
 
